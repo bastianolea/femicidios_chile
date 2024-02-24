805e5f5a21bc3852e325d226931417c778f9c602
Bastián Olea Herera <bastianolea@gmail.com>
2024-02-24 12:07:59 -0300

    avance

--- obtener_femicidios.R
+++ obtener_femicidios.R
@@ -1,7 +1,12 @@
 # http://www.nomasviolenciacontramujeres.cl/registro-de-femicidios/
 # registro realizado por la Red Chilena contra la Violencia hacia las Mujeres desde 2010 en adelante
 
-install.packages("googlesheets4")
+# este script obtiene los datos sobre femicidios en Chile y los descarga.
+# obtiene los enlaces con el paquete {rvest} haciendo web scraping del sitio de la Red,
+# luego descarga los datos desde Google Docs usando el paquete {googlesheets4}
+
+
+# install.packages("googlesheets4")
 library(googlesheets4)
 library(rvest)
 library(purrr)
@@ -10,40 +15,49 @@ library(janitor)
 library(lubridate)
 library(dplyr)
 
-# obtener enlaces ----
+#obtener enlaces ----
 
 #web scraping del sitio
 registro <- session("http://www.nomasviolenciacontramujeres.cl/registro-de-femicidios/")
 
-#obtener enlaces de google sheets
+#obtener enlaces a los datos, que están en google sheets
 registro_enlaces <- registro |> 
   read_html() |> 
   html_elements(".vce-col-inner") |> 
   html_elements("a") |> 
   html_attr("href")
 
-# descargar datos ----
 gs4_deauth() #usar google sheets sin cuenta
 
-# tabla <- read_sheet(registro_enlaces[14])
-#
-# metadatos <- gs4_get(registro_enlaces[13])
-# la tabla de 2024 viene en formato excel, así que no se puede obtener de la misma forma
+# read_sheet(registro_enlaces[1])
 
 # obtener todos los datos ----
 planillas <- map(registro_enlaces, ~{
-  # .x <- registro_enlaces[5]
-  message(.x)
-  metadatos <- gs4_get(.x) |> try()
+  message(glue("descargando archivo {.x}..."))
   
-  if (class(metadatos)[1] == "try-error") return(NULL) #salir si no se obtiene
+  #descargar planilla
+  datos <- read_sheet(.x) |> try()
   
-  nombre <- metadatos$sheets$name[1] |> tolower()
-  
-  datos <- read_sheet(metadatos)
-
-  return(datos)
+  if (class(datos)[1] == "try-error") {
+    return(NULL) #salir si no se obtiene archivo
+  } else {
+    return(datos)
+  }
 })
+
+# # obtener todos los datos ----
+# planillas <- map(registro_enlaces, ~{
+#   message(glue("descargando archivo{.x}..."))
+#   metadatos <- gs4_get(.x) |> try()
+#   
+#   if (class(metadatos)[1] == "try-error") return(NULL) #salir si no se obtiene archivo
+#   
+#   # nombre <- metadatos$sheets$name[1] |> tolower()  #obtener nombre de la hoja de la planilla
+#   
+#   datos <- read_sheet(metadatos)
+# 
+#   return(datos)
+# })
 
 
 # descargar datos 2024 ----
--- procesar_femicidios.R
+++ procesar_femicidios.R
@@ -1,4 +1,9 @@
+library(dplyr)
+library(purrr)
+library(glue)
+library(lubridate)
 library(tidyr)
+library(fs)
 
 # fechas <- map(datos[[2]], ~{
 #   if (class(.x)[1] == "POSIXct") {
@@ -48,13 +53,35 @@ walk(planillas_limpias, ~{
 
 
 # cargar todos ----
-library(fs)
+
 
 archivos <- dir_ls("datos")
 
 datos <- map(archivos, ~{
-  readxl::read_xlsx(.x) #|> 
-  # janitor::row_to_names(2)
-  })
+  readxl::read_xlsx(.x)
+  }) |> 
+  list_rbind()
 
-datos
+datos |> names()
+
+
+datos |> filter(is.na(id)) #hay casos repetidos
+
+datos |> filter(is.na(edad), is.na(edad_victima), is.na(edad_2))
+
+datos |> filter(is.na(lugar))
+datos |> count(lugar, region, comuna)
+
+datos |> filter(is.na(nombre_victima), is.na(nombre))
+
+
+datos |> mutate(fecha_2 = ymd(fecha)) |> 
+  filter(is.na(fecha_2))
+
+datos |> count(categorias_red_chilena)
+datos |> filter(is.na(categorias_red_chilena)) |> 
+  glimpse()
+datos |> count(categorias_red_chilena, categoria_red_chilena) |> print(n=Inf)
+
+datos |> filter(is.na(relacion_victima_femicida)) |> 
+  glimpse()
